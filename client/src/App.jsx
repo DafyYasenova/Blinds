7d19125087a03fbff145277842bf243941f7f438
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-02 14:40:41 +0200

    add not found component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import SideHeader from './components/SideHeader'
+import Register from './components/Register'
+import Login from './components/Login'
+import Create from './components/Create'
+import ContactUs from './components/ContactUs'
+import Welcome from './components/Welcome'
+import NotFound from './components/NotFound'
+import Details from './components/Details'
+import Catalog from './components/Catalog'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+
+    <main>
+        <SideHeader />
+
+        <Register />
+
+       <Login /> 
+
+       <Create />
+
+        <ContactUs />
+
+        <Welcome />
+        
+       <NotFound />
+
+        <Details />
+
+        <Catalog />
+        
+    </main> 
+
+    <Footer />
     </>
   )
 }
---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+export default function NotFound(){
+    return(
+        <section className="not-found">
+            
+        <img src="/Images/not-found.jpg" alt="{{image}}" />
+
+    </section>
+    )
+}
