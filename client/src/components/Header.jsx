ee4e3acfd44de3fc1da7563d7921a6b6afd91d61
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-02 14:34:38 +0200

    add header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,23 @@
+export default function Header(){
+    return(
+        <div>
+        <header>
+        <section className="navigation">
+            <p><img src="./Images/blinds.png" height="38px"/> <span>BLINDS</span></p>
+            <nav>
+                <ul>
+                    <li><a href="/about">ABOUT</a></li>
+                    <li><a href="/catalog">CATALOG</a></li>
+                    <li><a href="/create">CREATE</a></li>
+                    <li><a href="/users/login">LOGIN</a></li>
+                    <li><a href="/users/register">REGISTER</a></li>
+                    <li><a href="/users/logout">LOGOUT</a></li>
+                    <li><a href="/search">SEARCH</a></li>
+
+                </ul>
+            </nav>
+        </section>
+    </header>
+    </div>
+    )
+}
---
+++ client/src/components/SideHeader.jsx
@@ -0,0 +1,9 @@
+export default function SideHeader() {
+    return (
+        <section className="site-header">
+            <h1> A perfect solution for your interior</h1>
+            <button>Go to gallery</button>
+        </section>
+
+    )
+}
