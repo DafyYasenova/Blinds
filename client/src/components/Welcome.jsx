207a0f00c9a6973f30def362f72a9b1af6754285
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-02 14:36:32 +0200

    create welcome component

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,16 @@
+export default function Welcome(){
+    return (
+        <section className="welcome">
+        <h2>Welcome to the world of blinds</h2>
+        <p>Here you will find a wide variety of blinds, the perfect solution for your interior. We have a large
+            range of models, colors, fabrics and sizes. We work on individual projects, taking into account the
+            specific requirements of each.
+        </p>
+        <img src="/Images/foto-shtora.jpg" alt="{{image}}" />
+
+        <img src="/Images/shtora-harmoniya.jpg" alt="{{image}}" />
+        <img src="/Images/vertikalni-shtori.jpg" alt="{{image}}" />
+        <img src="/Images/perde-shtora.jpg" alt="{{image}}" />
+    </section>
+    )
+}
