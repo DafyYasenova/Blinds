96a868bdbff69c475c0b874af11aabc6f31f4758
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-09 08:00:21 +0200

    add search.module.css styles for search component

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,7 +1,9 @@
+import styles from './Search.module.css';
+
 export default function Search() {
     return (
-        <section className="search">
-            <section className="search-form">
+        <section className={styles.search}>
+            <section className={styles["search-form"]}>
                 <h2>Search</h2>
 
                 <form className="search">
@@ -14,34 +16,34 @@ export default function Search() {
                
 
                 <div id="search-container">
-                    <ul className="blind-wrapper">
+                    <ul className={styles["blind-wrapper"]}>
 
-                        <li className="blind">
+                        <li className={styles.blind}>
                              <img src="/Images/vertikalni-shtori.jpg" height="200px" alt="shtora" /> 
 
                             <p>
                                 <strong>Model: </strong><span className="model">Blinds</span>
                             </p>
 
-                            <a className="details-btn" href="">Details</a>
+                            <a className={styles["details-btn"]} href="">Details</a>
                         </li>
-                        <li className="blind">
+                        <li className={styles.blind}>
                              <img src="/Images/vertikalni-shtori.jpg" height="200px" alt="shtora" /> 
 
                             <p>
                                 <strong>Model: </strong><span className="model">Blinds</span>
                             </p>
 
-                            <a className="details-btn" href="">Details</a>
+                            <a className={styles["details-btn"]} href="">Details</a>
                         </li>
-                        <li className="blind">
+                        <li className={styles.blind}>
                              <img src="/Images/vertikalni-shtori.jpg" height="200px" alt="shtora" /> 
 
                             <p>
                                 <strong>Model: </strong><span className="model">Blinds</span>
                             </p>
 
-                            <a className="details-btn" href="">Details</a>
+                            <a className={styles["details-btn"]} href="">Details</a>
                         </li>
                     </ul>
 
