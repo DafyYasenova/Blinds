a19a97426161a6ab1e4b7ed0e344b910064e0d5f
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-09 11:30:32 +0200

    add isLoading component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,31 +13,38 @@ import Catalog from './components/Catalog/Catalog'
 import Footer from './components/Footer/Footer'
 import Search from './components/Search/Search'
 import Edit from './components/Edit/Edit'
+import Loading from './components/Loading/Loading'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
 
   return (
     <>
       <Header />
 
       <main>
+        {isLoading && <Loading />}
         <Routes>
-         <Route path='/' element={<Home />} />
-         <Route path='/register' element={<Register />} />
-         <Route path='/login' element={<Login />} />
-         <Route path='/contact-us' element={<ContactUs/>} />
-         <Route path='/about' element={<About />} />
-         <Route path='/not-found' element={<NotFound />} />
-         <Route path='/catalog' element={<Catalog />} />
-         <Route path='/create' element={<Create />} />
-         <Route path='/search' element={<Search />} />
-         <Route path='/details' element={<Details />} />
-         <Route path='/edit' element={<Edit />} />
-         <Route path='*' element={<NotFound />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/not-found' element={<NotFound />} />
+          <Route path='/catalog' element={<Catalog />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/details' element={<Details />} />
+          <Route path='/edit' element={<Edit />} />
+          <Route path='*' element={<NotFound />} />
 
 
-        
+
 
         </Routes>
 
---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading(){
+    return(
+        <div className="loading-container">
+        <div className="loading-spinner">
+          <span className="loading-spinner-text">Loading</span>
+        </div>
+      </div>
+    )
+}
