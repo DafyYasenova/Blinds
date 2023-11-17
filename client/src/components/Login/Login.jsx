e930e401bc680a40b4eb00c97b170f87169a9c96
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-17 12:06:56 +0200

    update login component and add css module

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,14 +1,17 @@
+import styles from './Login.module.css';
+import { Link } from 'react-router-dom'
+
 export default function Login(){
     return(
         <section id="login">
-            <div className="form">
+            <div className={styles.form}>
                 <h2>LOGIN</h2>
-                <form  className="login-form">
+                <form  className={styles["login-form"]}>
                     <input type="text" name="email" id="login-email" placeholder="email" />
                     <input type="password" name="password" id="login-password" placeholder="password" />
 
                     <button type="submit">login</button>
-                    <p className="message">Not registered? <a href="/register">Register now</a></p>
+                    <p className={styles.message}>Not registered? <Link to="/register">Register now</Link></p>
                 </form>
             </div>
         </section>
