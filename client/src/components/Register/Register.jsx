138fd5a1176cf5a7a15d273b8d77fb637ee35233
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-17 12:07:16 +0200

    update register component and add css module

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,14 +1,16 @@
+import styles from './Register.module.css';
+import { Link} from 'react-router-dom';
 export default function Register(){
     return (
         <section id="register">
-            <div className="form">
+            <div className={styles.form}>
                 <h2>REGISTER</h2>
-                <form className="register-form" >
+                <form className={styles["register-form"]} >
                     <input type="text" name="email" id="register-email" placeholder="email" />
                     <input type="password" name="password" id="register-password" placeholder="password" />
                     <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
                     <button type="submit">register</button>
-                    <p className="message">Already registered? <a href="/login">Login now</a></p>
+                    <p className={styles.message}>Already registered? <Link to="/login">Login now</Link></p>
                 </form>
             </div>
         </section>
