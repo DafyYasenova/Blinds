ea8ed0aee0c6d43081375e5b128b5d922e39fa76
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-19 13:13:24 +0200

    update loading spinner

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -3,10 +3,7 @@ import styles from './Loading.module.css';
 
 export default function Loading(){
     return(
-        <div className={styles["loading-container"]}>
-        <div className={styles["loading-spinner"]}>
-          <span className={styles["loading-spinner-text"]}>Loading</span>
-        </div>
-      </div>
+      
+      <div className={styles["lds-roller"]}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     )
 }
