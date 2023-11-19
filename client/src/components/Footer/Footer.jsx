398d693248d5e35e14fc8b441a5b52ad33b60558
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-19 18:22:48 +0200

    update footer
    icon path to contact-us page

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,21 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 import {faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
-
+import { Link } from 'react-router-dom' 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
 
             <ul>
 
+                 {/* <li><Link to="https://www.facebook.com/"><FontAwesomeIcon icon={faFacebook} /></Link></li> */}
                 <li><FontAwesomeIcon icon={faFacebook} /></li>
                 <li><FontAwesomeIcon icon={faTwitter} /></li>
                 <li><FontAwesomeIcon icon={faInstagram} /></li>
                 <li><FontAwesomeIcon icon={faGithub} /> </li>
-                <li><FontAwesomeIcon icon={faEnvelope} /> </li>
+                <li><Link to="/contact-us" ><FontAwesomeIcon icon={faEnvelope} /> </Link></li>
 
             </ul>
-            <p>&copy; 2023 Dafy Yasenova. All Rights Reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Dafy Yasenova. All Rights Reserved.</p>
 
         </footer>
     )
