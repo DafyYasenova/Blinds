e4dadb1c604a6c29724b7983fcbbc1874480f34e
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-21 07:57:09 +0200

    create custom hook useForm

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useForm(submitHandler, initilalValues) {
+    const [values, setValues] = useState(initilalValues);
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        submitHandler();
+    }
+    return {
+        values,
+        onChange,
+        onSubmit,
+    }
+}
