7711e3081232c9a3dacb2f83325e0da3a5245e1e
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-12-06 13:51:52 +0200

    update authContext with validate login and register

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -5,67 +5,251 @@ import { useNavigate } from 'react-router-dom'
 import usePersistedState from "../hooks/usePersistedState";
 
 
-
 const AuthContext = createContext();
 
 export const AuthProvider = ({
   children,
 
 }) => {
-  const [errors, setErrors] = useState({ email: false, password: false });
+  const [errors, setErrors] = useState({ email: false, password: false, });
+  const [errorsRegister, setErrorRegister] = useState({ email: false, password: false, repeat: false, username: false });
+  const [errorMessage, setErrorMessage] = useState("")
+ 
   const navigate = useNavigate();
 
   const [auth, setAuth] = usePersistedState('auth', {});
 
-  const onBlur = (values) => {
 
-    if (values.email.length < 9) {
-
-      setErrors(prevErrors => ({ ...prevErrors, email: true }));
+  const validate = (values) => { 
+    if (!values.password && !values.email && !values.repeat && !values.username) {
+      setErrorMessage("Some field is empty")
+      setErrors(prevErrors => ({ ...prevErrors, password: true, email: true }));
+      setTimeout(() => {
+        setErrorMessage('')
+        setErrors((prevErrors) =>
+          ({ ...prevErrors, password: false, email: false, repeat: false, username: false }))
+      }, 2000)
+      throw new Error;
+    }
+    if (!values.email) {
+      setErrors((prevErrors) =>
+        ({ ...prevErrors, email: true }))
+      setErrorMessage("Some field is empty")
 
       setTimeout(() => {
         setErrors((prevErrors) =>
           ({ ...prevErrors, email: false }))
+        setErrorMessage('')
       }, 2000)
-      return;
-
-    } else {
-      setErrors(prevErrors => ({ ...prevErrors, email: false }));
+      throw new Error;
     }
 
-    if (values.password.length < 6) {
+    if (values.email.length < 9) {
 
+      setErrors((prevErrors) =>
+        ({ ...prevErrors, email: true }))
+
+      setErrorMessage("Minimum length is 9 characters")
+      setTimeout(() => {
+        setErrors((prevErrors) =>
+          ({ ...prevErrors, email: false }))
+        setErrorMessage('')
+      }, 2000)
+      throw new Error;
+
+    }
+    if (!values.password) {
+      setErrorMessage("Some field is empty")
       setErrors(prevErrors => ({ ...prevErrors, password: true }));
-      return;
-    } else {
-      setErrors(prevErrors => ({ ...prevErrors, password: false }));
+      setTimeout(() => {
+        setErrorMessage('')
+        setErrors((prevErrors) =>
+          ({ ...prevErrors, password: false }))
+      }, 2000)
+      throw new Error;
+    }
+    if (values.password.length < 6) {
+      setErrorMessage("Minimum length is 6 characters")
+      setErrors(prevErrors => ({ ...prevErrors, password: true }));
+
+      setTimeout(() => {
+        setErrorMessage('')
+        setErrors((prevErrors) =>
+          ({ ...prevErrors, password: false }))
+      }, 2000)
+      throw new Error;
+    }
+
+  };
+
+  const validateRegister = (values,confirmPassword) => {
+    if (!values.email) {
+      setErrorRegister((prevErrors) =>
+        ({ ...prevErrors, email: true }))
+      setErrorMessage("Some field is empty")
+
+      setTimeout(() => {
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, email: false }))
+        setErrorMessage('')
+      }, 2000)
+
+    throw new Error;
+    } 
+    if (values.email.length < 9) {
+
+      setErrorRegister((prevErrors) =>
+        ({ ...prevErrors, email: true }))
+
+      setErrorMessage("Minimum length is 9 characters")
+      setTimeout(() => {
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, email: false }))
+        setErrorMessage('')
+      }, 2000)
+      throw new Error;
+
+    }
+    if (!values.username) {
+      setErrorRegister((prevErrors) =>
+        ({ ...prevErrors, username: true }))
+      setErrorMessage("Some field is empty")
+
+
+      setTimeout(() => {
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, username: false }))
+        setErrorMessage('')
+      }, 2000)
+      throw new Error;
+    }
+    if (values.username.length < 3 || values.username.length > 20) {
+
+      setErrorRegister((prevErrors) =>
+        ({ ...prevErrors, username: true }))
+
+      setErrorMessage("Minimum length is 3 characters")
+      setTimeout(() => {
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, username: false }))
+        setErrorMessage('')
+      }, 2000)
+      throw new Error;
+
+    }
+   
+    if (!values.password) {
+      setErrorMessage("Some field is empty")
+      setErrorRegister(prevErrors => ({ ...prevErrors, password: true }));
+      setTimeout(() => {
+        setErrorMessage('')
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, password: false }))
+      }, 2000)
+      throw new Error;
+    }
+    if (values.password!==confirmPassword) {
+      setErrorMessage("Please entry correct password")
+      setErrorRegister(prevErrors => ({ ...prevErrors, password: true }));
+      setTimeout(() => {
+        setErrorMessage('')
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, password: false }))
+      }, 2000)
+      throw new Error;
+    }
+    if (values.password.length < 6) {
+      setErrorMessage("Minimum length is 6 characters")
+      setErrorRegister(prevErrors => ({ ...prevErrors, password: true }));
+
+      setTimeout(() => {
+        setErrorMessage('')
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, password: false }))
+      }, 2000)
+      throw new Error;
+    }
+    if (confirmPassword.length < 6) {
+      setErrorRegister((prevErrors) =>
+        ({ ...prevErrors, repeat: true }))
+      setErrorMessage("Minimum length is 6 characters")
+
+      setTimeout(() => {
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, repeat: false }))
+        setErrorMessage('')
+      }, 2000)
+      throw new Error;
+    }
+
+    if (!confirmPassword) {
+      setErrorRegister((prevErrors) =>
+        ({ ...prevErrors, repeat: true }))
+      setErrorMessage("Some field is empty")
+
+      setTimeout(() => {
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, repeat: false }))
+        setErrorMessage('')
+      }, 2000)
+      throw new Error;
     }
 
 
+    if (!values.password && !values.email && !values.repeat && !values.username) { 
+      setErrorMessage("Some field is empty")
+      setErrorRegister(prevErrors => ({ ...prevErrors, password: true, email: true, repeat: true, username: true }));
+      setTimeout(() => {
+        setErrorMessage('')
+        setErrorRegister((prevErrors) =>
+          ({ ...prevErrors, password: false, email: false, repeat: false, username: false }))
+      }, 2000)
+      throw new Error;
+    }
 
   }
 
-
   const loginSubmitHandler = async (values) => {
+    const trimmedRegisterData = {};
+    Object.keys(values).forEach(key => {
+        trimmedRegisterData[key] = values[key].trim();
+    });
     try {
-      onBlur(values)
-      const result = await authService.login(values.email, values.password);
+
+      validate(values)
+      const result = await authService.login(trimmedRegisterData.email, trimmedRegisterData.password);
       setAuth(result);
 
       localStorage.setItem('accessToken', result.accessToken);
       navigate('/');
+
     } catch (error) {
       return error
     }
   };
 
   const registerSubmitHandler = async (values) => {
+    const { confirmPassword, ...registerData } = values;
 
-    const result = await authService.register(values.email, values.password, values.username)
+    const trimmedRegisterData = {};
+
+    Object.keys(registerData).forEach(key => {
+        trimmedRegisterData[key] = registerData[key].trim();
+    });
+
+  try{
+  
+    validateRegister(trimmedRegisterData,confirmPassword)
+
+    const result = await authService.register(trimmedRegisterData.email, trimmedRegisterData.password, trimmedRegisterData.username)
     setAuth(result);
 
     localStorage.setItem('accessToken', result.accessToken);
     navigate('/');
+  }catch (error) {
+    return error;
+  }
+ 
   };
 
   const logoutHandler = () => {
@@ -82,7 +266,10 @@ export const AuthProvider = ({
     email: auth.email,
     userId: auth._id,
     isAuthenticated: !!auth.accessToken,
-    errors
+    errors,
+    errorMessage,
+    errorsRegister
+
   };
   return (
     <AuthContext.Provider value={values}>
